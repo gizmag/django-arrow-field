161876589c59873cb01a8166e6ddaff286564148
Jacob Haslehurst <jacob@haslehurst.net>
2014-02-14 12:58:16 +1100

    woops forgot an __init__.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-arrow-field',
-    version='0.1.0',
+    version='0.1.1',
     description='Django arrow datetime field',
     author='Gizmag',
     author_email='tech@gizmag.com',
