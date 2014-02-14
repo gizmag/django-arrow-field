3f920a93ad37d686fe6cc67e9735d9e865bc0521
Jacob Haslehurst <jacob@haslehurst.net>
2014-02-14 12:49:35 +1100

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-arrow-field',
+    version='0.1.0',
+    description='Django arrow datetime field',
+    author='Gizmag',
+    author_email='tech@gizmag.com',
+    url='https://github.com/gizmag/django-arrow-field',
+    packages=find_packages(),
+    install_requires=['django', 'arrow']
+)
