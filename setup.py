5303b4e30d4723ee0ca228dc4e6abe467416bf28
Jacob Haslehurst <jacob@haslehurst.net>
2014-02-17 12:45:40 +1100

    version 0.2.0 release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-arrow-field',
-    version='0.1.1',
+    version='0.2.0',
     description='Django arrow datetime field',
     author='Gizmag',
     author_email='tech@gizmag.com',
