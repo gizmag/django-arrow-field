f1e3e74e9fbeb4a9bea9b54becedb0e0e35e8e8c
Jacob Haslehurst <jacob@haslehurst.net>
2014-02-17 11:45:40 +1100

    added input formats to ArrowField

--- arrow_field/form_fields.py
+++ arrow_field/form_fields.py
@@ -4,6 +4,18 @@ from django.forms import DateTimeField
 
 
 class ArrowField(DateTimeField):
+    input_formats = [
+        'YYYY-MM-DD HH:mm:ss',
+        'YYYY-MM-DD HH:mm',
+        'YYYY-MM-DD',
+        'MM/DD/YYYY HH:mm:ss',
+        'MM/DD/YYYY HH:mm',
+        'MM/DD/YYYY',
+        'MM/DD/YY HH:mm:ss',
+        'MM/DD/YY HH:mm',
+        'MM/DD/YY',
+    ]
+
     def to_python(self, value):
         if isinstance(value, arrow.Arrow):
             return value
