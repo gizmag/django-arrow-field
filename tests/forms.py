97b7fe0f9bed6a792749afdc43c9ad260a31d23b
Florian Klink <f.klink@nynex.de>
2016-03-23 12:50:13 +0100

    support django 1.9
    set fields explicitly, remove unused import (from|to)_current_timezone

--- tests/forms.py
+++ tests/forms.py
@@ -7,9 +7,11 @@ from arrow_field.form_fields import ISO8601ArrowField
 class PersonForm(ModelForm):
     class Meta:
         model = Person
+        fields = ['first_name', 'birthday']
 
 
 class ISO8601PersonForm(ModelForm):
     birthday = ISO8601ArrowField()
     class Meta:
         model = Person
+        fields = ['first_name', 'birthday']
